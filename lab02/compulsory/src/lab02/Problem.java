9be048855aa06844afe2a7de1f6eff01812eef0e
PanzariuAdi <panzariu.adi00@gmail.com>
2021-02-25 09:12:27 +0200

    Update Problem.java

--- lab02/compulsory/src/lab02/Problem.java
+++ lab02/compulsory/src/lab02/Problem.java
@@ -1,4 +1,36 @@
 package lab02;
 
 public class Problem {
+    private Source sources[];
+    private Destination destinations[];
+    private int destNumber, sourceNumber;
+    private int distances[][];
+
+    Problem() {
+        this.destNumber = 0;
+        this.sourceNumber = 0;
+    }
+
+    public void setDestinations(Destination[] destinations) {
+        this.destinations = destinations;
+        this.destNumber = destinations.length;
+    }
+
+    public void setSources(Source[] sources) {
+        this.sources = sources;
+        this.sourceNumber = sources.length;
+    }
+
+    public void addDestination(Destination dest) {
+        destinations[destNumber++] = dest;
+    }
+
+    public void addSource(Source source) {
+        sources[sourceNumber++] = source;
+    }
+
+    public void setDistances(int[][] distances) {
+        this.distances = distances;
+    }
+
 }
