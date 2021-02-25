28cdbcfe1c63fe48dc96ce5f5b0f0345186549df
PanzariuAdi <panzariu.adi00@gmail.com>
2021-02-25 09:30:45 +0200

    Update Destination.java

--- lab02/compulsory/src/lab02/Destination.java
+++ lab02/compulsory/src/lab02/Destination.java
@@ -2,6 +2,7 @@ package lab02;
 
 public class Destination {
     private String name;
+    private int demand;
 
     public Destination(String name) {
         this.name = name;
@@ -13,6 +14,14 @@ public class Destination {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getDemand() {
+        return demand;
+    }
+
+    public void setDemand(int demand) {
+        this.demand = demand;
     }
 
     @Override
