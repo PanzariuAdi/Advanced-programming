9d5e928844de381734551e7d408a33555adc69cd
PanzariuAdi <panzariu.adi00@gmail.com>
2021-03-11 09:07:39 +0200

    Update Visitable.java

--- lab03/optional/src/lab03/Visitable.java
+++ lab03/optional/src/lab03/Visitable.java
@@ -4,12 +4,9 @@ import java.time.Duration;
 import java.time.LocalTime;
 
 public interface Visitable {
-    LocalTime getOpenTime();
-    LocalTime getCloseTime();
-
-//    Create default methods in the interface Visitable, with the opening hour 09:30 and the closing hour 20:00.
-    default LocalTime DefaultOpenTime() { return LocalTime.of(9, 30); }
-    default LocalTime DefaultCloseTime() { return LocalTime.of(20, 0); }
+    //    Create default methods in the interface Visitable, with the opening hour 09:30 and the closing hour 20:00.
+    default LocalTime getOpenTime() { return LocalTime.of(9, 30); }
+    default LocalTime getCloseTime() { return LocalTime.of(20, 0); }
 
     //   Create a static method getVisitingDuration,in the interface Visitable, that returns a Duration object, representing how long a location is opened during a day.
     static Duration getVisitingDuration(Duration duration, Visitable l1) {
